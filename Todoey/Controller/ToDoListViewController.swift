e05a8719a5dc06212a9e98ef24f9e9a7806f7175
Sushen <sat0006@jmss.vic.edu.au>
2018-07-20 12:09:50 +1000

    Persisting data using code able protocol

---
+++ Todoey/Controller/ToDoListViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by sushen satturu on 19/7/18.
+//  Copyright © 2018 Sushen Satturu. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        loadItems()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:- tableView data source methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+        
+    }
+    
+    //MARK:- tableView delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    //MARK:- add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        
+        let confirmAction = UIAlertAction(title: "Add Item", style: .default) { (uiAlertAction) in
+            let text = textField.text
+            
+            let newItem = Item()
+            newItem.title = text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+            
+            
+            
+        }
+        
+        let cancleAction = UIAlertAction(title: "Cancle", style: .cancel) { (uiAlertAction) in}
+
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(cancleAction)
+        alert.addAction(confirmAction)
+        
+        present(alert, animated: true) {
+            
+        }
+        
+        
+        
+    }
+    
+    //MARK:- data manipulation methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array \(error)")
+        }
+        
+        tableView.reloadData()
+
+    }
+    
+    func loadItems() {
+        
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            
+            do {
+            itemArray = try decoder.decode([Item].self , from: data)
+            }
+            catch {
+                print("Error decoding item \(error)")
+            }
+        }
+        
+    }
+    
+
+
+}
+
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by sushen satturu on 19/7/18.
+//  Copyright © 2018 Sushen Satturu. All rights reserved.
+//
+
+import Foundation
+
+class Item : Encodable, Decodable {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
